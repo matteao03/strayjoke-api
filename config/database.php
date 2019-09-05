18a3dbfd9327f8616dfe2e9dc966731cb7ecbaea
matteao <matteao_03@163.com>
2019-09-05 08:08:22 +0800

    config

--- config/database.php
+++ config/database.php
@@ -44,13 +44,20 @@ return [
         ],
 
         'mysql' => [
+            'read' => [
+                'host' => ['47.100.47.177'],
+                'port' => env('DB_PORT', '3306'),
+            ],
+            'write' => [
+                'host' => ['47.100.47.177'],
+                'port' => env('DB_PORT', '3307'),
+            ],
+            'sticky'    => true,
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'strayjoke'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'root'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
